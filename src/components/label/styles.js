e138d4517d7d848a0e672ab7c6b807756214900a
Martin Hochenwarter <m.hochenwarter@gmail.com>
2020-11-04 11:53:28 +0100

    Fix label width for native and web

--- src/components/label/styles.js
+++ src/components/label/styles.js
@@ -1,17 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 
 export default StyleSheet.create({
   container: {
     position: 'absolute',
     top: 0,
-    left: '-50%',
-    width: '100%',
-    paddingLeft: '50%',
+    ...(Platform.select({ web: {
+      left: '-100%',
+      width: '200%',
+      paddingLeft: '100%',
+    },
+    default: {
+      left: '-50%',
+      width: '100%',
+      paddingLeft: '50%',
+    } })),
   },
 
   text: {
     textAlign: 'left',
     includeFontPadding: false,
     textAlignVertical: 'top',
+    ...(Platform.select({ web: {
+      width: '100%',
+    },
+    default: {
+      width: '200%',
+    } })),
   },
 });
